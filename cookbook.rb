e1eb9af4c2f7cb5c9afced9c02e91ecf1b998cb9
Rogério A. Ferreira <60974109+roferreira87@users.noreply.github.com>
2020-04-04 14:17:28 -0300

    Update cookbook.rb
    alteração na de concatenação para interpolação (é preciso aspas duplas).

--- cookbook.rb
+++ cookbook.rb
@@ -3,4 +3,4 @@ puts "Be-vindo ao Cookbook, sua rede social de receitas"
 puts "Digite o nome da receita: "
 nome = gets.chomp()
 
-puts"Receita " + nome + " cadastrada com sucesso!"
+puts"Receita #{nome} cadastrada com sucesso!"
