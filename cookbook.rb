42f4ca9fa83ab7ec50058db1cc50b7cf0168d312
Rogério A. Ferreira <60974109+roferreira87@users.noreply.github.com>
2020-04-04 18:11:28 -0300

    Update cookbook.rb
    aula 03 - Arrey

--- cookbook.rb
+++ cookbook.rb
@@ -1,6 +1,10 @@
 puts "Be-vindo ao Cookbook, sua rede social de receitas"
 
+receitas = []
+
 puts "Digite o nome da receita: "
 nome = gets.chomp()
+
+receita << nome
 
 puts"Receita #{nome} cadastrada com sucesso!"
