edd1e8e6c30015483e0d27b81c0afe1d7d8d430e
Rogério A. Ferreira <60974109+roferreira87@users.noreply.github.com>
2020-04-04 19:09:24 -0300

    Estruturas de repetição
    aula 04 - estruturas finitas como for e o each
    e as que podem não ter condições de saida como while

--- cookbook.rb
+++ cookbook.rb
@@ -2,9 +2,27 @@ puts "Be-vindo ao Cookbook, sua rede social de receitas"
 
 receitas = []
 
-puts "Digite o nome da receita: "
-nome = gets.chomp()
+while(true) do
+    puts "Digite o nome da receita: "
+    nome = gets.chomp()
 
-receitas << nome
+    receitas << nome
+    puts
+    puts"Receita #{nome} cadastrada com sucesso!"
+    puts
+    puts "======== Receitas cadastradas ========"
+    puts
 
-puts"Receita #{nome} cadastrada com sucesso!"
+    # aqui é um for em ruby
+    # for receita in receitas do 
+    #   puts receita
+    # end
+    # ou
+    # utilize o metodo .each
+    # receitas.each do |receita|
+    #   puts receita
+    # end
+
+    puts receitas
+    puts
+end
